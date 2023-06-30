7be60b33d05dfce6794fecf382bc05df0b320a00
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-30 18:43:52 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,7 +4,69 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// const modal=document.querySelector('#modal')
+// modal.classList.add('hidden')
 
+const like=document.getElementsByClassName('like-glyph')
+console.log(like)
+
+Array.from(like).forEach(function(eachBtn) {
+  // Your code here for each element
+eachBtn.addEventListener('click',()=>{
+
+  mimicServerCall()
+  .then( ()=>{
+
+  if(eachBtn.innerHTML==='♡'){
+    eachBtn.innerHTML='♥'
+    eachBtn.classList.add('activated-heart')
+  }
+  else{
+    eachBtn.innerHTML='♡'
+    eachBtn.classList.remove('activated-heart')
+  }
+    
+  })
+  .catch(error => {
+    const modal2=document.getElementByid('modal')
+    modal2.classList.remove('hidden')
+    const modalMessage=document.querySelector('#modal-message')
+    console.log(modalMessage)
+    modalMessage.innerHTML=error
+    .setTimeout(function(){
+      
+      modal2.classList.add('hidden')
+    },3000)
+  })
+})
+});
+// like.forEach(eachBtn => console.log(eachBtn))
+
+// like.forEach(eachBtn => eachBtn.addEventListener('click',()=>{
+
+//   mimicServerCall()
+//   .then(data =>{
+
+//   if(eachBtn.innerHTML==='♡'){
+//     eachBtn.innerHTML='♥'
+//     eachBtn.classList.add('activated-heart')
+//   }
+//   else{
+//     eachBtn.innerHTML='♡'
+//     eachBtn.classList.remove('activated-heart')
+//   }
+    
+//   })
+//   .catch(error => {
+//     // const modal=document.querySelector('#modal')
+//     modal.classList.remove('hidden')
+//     const modalMessage=document.querySelector('#modal-message')
+//     modalMessage.textContent=error
+//     .setTimeout(function(){
+//       modal.classList.add('hidden')
+//     },3000)
+//   })
+// }))
 
 
 //------------------------------------------------------------------------------
